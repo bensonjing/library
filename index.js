5c7a0f21bc3ee80595e6c2f283c3eebf98b16633
Benson Jing <bensonjing23@gmail.com>
2022-07-13 17:06:10 -0400

    dev: add js function that display books onto the screen

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const library = document.querySelector(".library");
+
 let myLibrary = [];
 
 function Book(title, author, pages, haveRead) {
@@ -10,4 +12,32 @@ function Book(title, author, pages, haveRead) {
 function addBookToLibrary(title, author, pages, haveRead) {
   const newBook = Book(title, author, pages, haveRead);
   myLibrary.push(newBook);
+}
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    console.log(myLibrary);
+    console.log(book);
+    const bookDiv = document.createElement("div");
+
+    const title = document.createElement("div");
+    const author = document.createElement("div");
+    const pages = document.createElement("div");
+    const haveRead = document.createElement("button");
+    const remove = document.createElement("button");
+
+    title.textContent = `"${book.title}"`;
+    author.textContent = book.author;
+    pages.textContent = `${book.pages} pages`;
+    haveRead.textContent = book.haveRead ? "Read" : "Not Read";
+    remove.textContent = "Remove";
+
+    bookDiv.appendChild(title);
+    bookDiv.appendChild(author);
+    bookDiv.appendChild(pages);
+    bookDiv.appendChild(haveRead);
+    bookDiv.appendChild(remove);
+
+    library.appendChild(bookDiv);
+  });
 }
